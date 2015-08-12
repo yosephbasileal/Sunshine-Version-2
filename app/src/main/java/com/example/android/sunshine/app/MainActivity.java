82307cd59b6e871700068d220fd6fed9a032807c
Basileal Imana <yosephbasileal@yahoo.com>
2015-08-12 03:54:02 -0400

    created a list view of dummy data, updated icon

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.android.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -57,6 +63,37 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            //once the root view for the fragment has been created, it is time to populate
+            //the ListView with some dummy data "day, whether, high/low"
+            String[] forecastArray = {
+                    "Today - Sunny - 88/63",
+                    "Tomorrow - Foggy - 70/40",
+                    "Weds - Cloudy - 72/63",
+                    "Thurs - Asteroids - 75/65",
+                    "Fri - Heavy Rain - 65/56",
+                    "Sat - HELP TRAPPED IN WEATHERSTATION - 60/51",
+                    "Sun - Sunny - 80/68"
+            };
+
+            //create an ArrayList of dummy data
+            List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+
+            //initialize adapter
+            ArrayAdapter<String> mForecastAdapter =
+                    new ArrayAdapter<String>(
+                            //the current context (this fragment's parent activity)
+                            getActivity(),
+                            //ID of list item layout
+                            R.layout.list_item_forecast,
+                            //ID of the textview to populate
+                            R.id.list_item_forecast_textview,
+                            //forecast data
+                            weekForecast);
+            //get reference to the ListView, and attach this adapter to it
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(mForecastAdapter);
+
             return rootView;
         }
     }
