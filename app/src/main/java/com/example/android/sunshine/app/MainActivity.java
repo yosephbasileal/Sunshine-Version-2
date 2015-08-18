8af45c380811ea7cf6dd75f4b20f318737dd203e
Basileal Imana <yosephbasileal@yahoo.com>
2015-08-18 01:40:39 -0400

    modify UI to match mocks

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -54,6 +54,7 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
             }
         } else {
             mTwoPane = false;
+            getSupportActionBar().setElevation(0f);
         }
 
         ForecastFragment forecastFragment =  ((ForecastFragment)getSupportFragmentManager()
