ef7879b1b1ec7b4cf5ca623ffbcb7880e007efd8
Basileal Imana <yosephbasileal@yahoo.com>
2015-08-18 18:26:05 -0400

    now SyncAdapter is used to fetch data with periodic and immediate sync calls

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -23,6 +23,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.android.sunshine.app.sync.SunshineSyncAdapter;
+
 public class MainActivity extends ActionBarActivity implements ForecastFragment.Callback{
 
     // Get name of class programatically so that we don't have to hard code, it
@@ -69,6 +71,8 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
         ForecastFragment forecastFragment =  ((ForecastFragment)getSupportFragmentManager()
                          .findFragmentById(R.id.fragment_forecast));
                  forecastFragment.setUseTodayLayout(!mTwoPane);
+
+        SunshineSyncAdapter.initializeSyncAdapter(this);
 
     }
 
