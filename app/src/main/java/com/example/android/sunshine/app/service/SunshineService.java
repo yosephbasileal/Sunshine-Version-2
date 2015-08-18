195fa15b58db6af9003f0ee851c8ce4ba76fb722
Basileal Imana <yosephbasileal@yahoo.com>
2015-08-18 17:57:56 -0400

    added an alarm manager to start our intent service using a broadcast reciever

--- app/src/main/java/com/example/android/sunshine/app/service/SunshineService.java
+++ app/src/main/java/com/example/android/sunshine/app/service/SunshineService.java
@@ -17,8 +17,10 @@
 package com.example.android.sunshine.app.service;
 
 import android.app.IntentService;
+import android.content.BroadcastReceiver;
 import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -331,4 +333,13 @@ public class SunshineService extends IntentService {
         return locationId;
     }
 
+    public static class AlarmReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Intent sendIntent = new Intent(context, SunshineService.class);
+            sendIntent.putExtra(SunshineService.LOCATION_QUERY_EXTRA,intent.getStringExtra(SunshineService.LOCATION_QUERY_EXTRA));
+            context.startService(sendIntent);
+        }
+    }
 }
